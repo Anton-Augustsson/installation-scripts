905169463fa4ed5346a037c0c93404e2be419774
anton <anton.augustsson99@gmail.com>
2020-02-13 19:45:45 +0100

    try fix problem with black screen ofter boot

--- arch-linux.sh
+++ arch-linux.sh
@@ -46,26 +46,36 @@ KEYMAP=sv-latin1
 ' >>/etc/vconsole.conf
 source /etc/vconsole.conf
 
+#LANGUAGE=en_GB:en #untested
+#export LC_ALL="en_GB.UTF-8"
+export LC_MESSAGES="en_GB.UTF-8"
+export LC_COLLATE="en_GB.UTF-8"
+
 ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 hwclock --systohc
+
 
 ## Mirroring list
 pacman -S --noconfirm pacman-contrib
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
+
 ## Network
 pacman -S --noconfirm jre-openjdk networkmanager
 systemctl enable NetworkManager
+
 ### ssh
 pacman -S --noconfirm openssh
 systemctl enable sshd
 systemctl start sshd
 
+
 ## Boot loader
 pacman -S --noconfirm grub
 grub-install $DRIVE
 grub-mkconfig -o /boot/grub/grub.cfg
+
 
 ## Directory anton home
 cd /home/anton
@@ -108,40 +118,23 @@ systemctl start org.cups.cupsd.service
 sudo -u anton yay -S brother-mfc-l2700dw
 
 ## configuration files
-#scp anton@192.168.1.210:/plex/other/mountain1.jpg /home/anton/Pictures/wallpaper/wallpaper.jpg ~
-wget -q https://github.com/anton-1999/installation-scripts/raw/master/united/wallpaper.jpg -O ~/Pictures/wallpaper/wallpaper.jpg
+wget -q https://github.com/anton-1999/installation-scripts/raw/master/united/wallpaper.jpg -O /home/anton/Pictures/wallpaper/wallpaper.jpg
 #betterlockscreen -u /home/anton/Pictures/wallpaper/wallpaper.jpg
 
-#wget -q https://raw.githubusercontent.com/anton-1999/configuration-files/master/config/.zshrc -O ~/.zshrc
-#wget -q https://raw.githubusercontent.com/anton-1999/configuration-files/master/config/.emacs -O ~/.emacs
-#wget -q https://raw.githubusercontent.com/anton-1999/configuration-files/master/config/.Xdefaults -O ~/.Xdefaults
-#wget -q https://github.com/anton-1999/configuration-files/blob/master/config/.config/i3/config -O ~/.config/i3/config
-#wget -q https://raw.githubusercontent.com/anton-1999/configuration-files/master/config/.config/polybar/config -O ~/.config/polybar/config
-#wget -q https://raw.githubusercontent.com/anton-1999/configuration-files/master/config/.config/polybar/launch.sh -O ~/.config/polybar/launch.sh
-#wget -q https://raw.githubusercontent.com/anton-1999/configuration-files/master/config/.config/ranger/rc.conf -O ~/.config/ranger/rc.conf
-#wget -q https://raw.githubusercontent.com/anton-1999/configuration-files/master/config/.config/zathura/zathurarc -O ~/.config/zathura/zathurarc
-
-#cd /home/anton
-#git clone https://github.com/anton-1999/dotfiles.git
-#cd dotfiles
-#sudo -u anton stow i3 polybar sxhkd urxvt emacs bash zsh ranger zathura 
+cd /home/anton
+git clone https://github.com/anton-1999/dotfiles.git
+cd dotfiles
+rm /home/anton/.bashrc
+sudo -u anton stow urxvt emacs zsh ranger zathura 
+# desktop enviroment breakes possibly because of:  i3 polybar sxhkd bash
 
 ## Programing languge
-#pacman -S ispell ghc ghc-static cabal-install texlive-most
-#echo "export EDITOR=/usr/bin/emacs" >> ~/.zshrc
-
-### Haskell
-#sudo -u anton mkdir haskell
-#cd  haskell
-#sudo -u anton cabal update
-#sudo -u anton cabal user-config update
-#sudo -u anton cabal init 
-#sudo -u anton cabal configure --disable-library-vanilla --enable-shared --enable-executable-dynamic --ghc-options=-dynamic
-
+pacman -S ispell texlive-most
+echo "export EDITOR=/usr/bin/emacs" >> ~/.zshrc
 
 ## desktop language
-sudo -u anton localectl set-keymap se
-sudo -u anton localectl set-x11-keymap se
+localectl set-keymap se
+#localectl set-x11-keymap se  #no command found 
 
 # End
 echo '
