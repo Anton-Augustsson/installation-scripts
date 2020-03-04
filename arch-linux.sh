1116f1559b6195ed740f8927792397706399c96a
anton-1999 <anton.augustsson99@gmail.com>
2020-03-04 21:33:33 +0100

    Works to a sertent extent but urxt faild, stow is not tested yet

--- arch-linux.sh
+++ arch-linux.sh
@@ -93,12 +93,14 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 # Desktop environment
-pacman -S --noconfirm dialog wpa_supplicant xorg xorg-xinit xorg-server lightdm lightdm-gtk-greeter i3-gaps i3status dmenu feh alsa-utils powerline w3m scrot sxhkd brightnessctl stow
+pacman -S --noconfirm dialog wpa_supplicant xorg xorg-xinit xorg-server lightdm lightdm-gtk-greeter i3-gaps i3status dmenu feh alsa-utils w3m scrot sxhkd brightnessctl stow
 systemctl enable lightdm
 
+## terminal
+pacman -S --noconfirm rxvt-unicode powerline ranger zathura zathura-pdf-mupdf zathura-djvu
 
 ## Applications
-pacman -S --noconfirm rxvt-unicode firefox ranger nautilus arduino kicad openscad zathura zathura-pdf-mupdf zathura-djvu dia cura deluge 
+pacman -S --noconfirm firefox nautilus arduino kicad openscad dia cura deluge  # unexpected error? somthing with python, urxvt fails.
 
 ## yay
 cd /home/$USER/Programs
@@ -115,10 +117,10 @@ sudo -u $USER yay -S --noconfirm siji termsyn-font polybar betterlockscreen
 pacman -S --noconfirm cups cups-pdf gtk3-print-backends system-config-printer
 systemctl enable org.cups.cupsd.service
 systemctl start org.cups.cupsd.service
-sudo -u $$USER yay -S --noconfirm brother-mfc-l2700dw
+sudo -u $USER yay -S --noconfirm brother-mfc-l2700dw
 
 ## configuration files
-wget -q https://github.com/Anton-Augustsson/installation-scripts/raw/master/united/wallpaper.jpg -O /home/anton/Pictures/wallpaper/wallpaper.jpg
+wget -q https://github.com/Anton-Augustsson/installation-scripts/raw/master/united/wallpaper.jpg -O /home/$USER/Pictures/wallpaper/wallpaper.jpg
 #betterlockscreen -u /home/anton/Pictures/wallpaper/wallpaper.jpg  # Dosent work it probably desktop enviroment is needed
 
 ### Stow
