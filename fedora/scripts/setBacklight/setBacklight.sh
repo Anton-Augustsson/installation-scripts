f6b4a3d490f063fefde6f15116442f5c374b5d9d
Anton-Augustsson <Anton.Augustsson.4804@student.uu.se>
2022-07-25 14:47:00 +0200

    ranger and backlight for fedora

--- fedora/scripts/setBacklight/setBacklight.sh
+++ fedora/scripts/setBacklight/setBacklight.sh
@@ -2,10 +2,11 @@
 
 device='tpacpi::kbd_backlight'
 default_brightness=0
-current_brightness=$(sudo brightnessctl --device="$device" | grep "Current brightness" | awk '{print $3}')
+current_brightness=$(brightnessctl --device="$device" | grep "Current brightness" | awk '{print $3}')
 
 setBacklight() {
-	sudo brightnessctl --device="$device" set $1
+	#sudo brightnessctl --device="$device" set $1
+	brightnessctl --device="$device" set $1
 }
 
 if [ $current_brightness == $default_brightness ]
