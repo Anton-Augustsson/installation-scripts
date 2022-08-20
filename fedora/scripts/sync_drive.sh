3dff23d75f82114bccf1d42377ce3932ffc99193
Anton-Augustsson <anton.augustsson99@gmail.com>
2022-08-20 14:01:11 +0200

    rclone should be run for every kind of event in the Drive folder

--- fedora/scripts/sync_drive.sh
+++ fedora/scripts/sync_drive.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #https://unix.stackexchange.com/questions/24026/how-to-run-a-command-when-a-directorys-contents-are-updated
 dir1=$HOME/Drive/
-#rclone sync remote:backup $dir1
-while inotifywait -re delete -re modify $dir1; do
+dir_backup=$HOME/Drive_backup/
+cp -rf dir1 dir_backup # Safty backup
+rclone sync remote:backup $dir1
+while inotifywait -re delete -re modify -re create -re move $dir1; do
    rclone sync $dir1 remote:backup 
 done
