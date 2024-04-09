dce05bd9befc4b4b3e152ab57b46aeb1e0684857
Anton Augustsson <anton.augustsson99@gmail.com>
2024-04-09 17:34:04 +0200

    flatpak and docker/kind seems to work

--- opensuse.sh
+++ opensuse.sh
@@ -7,19 +7,18 @@
 
 requiring_manual_action_end_messages=()
 commands=(
-    "Setup Git"
     "Setup ZSH"
-    "Setup VSCode"
-    "Setup VSCode"
-    "Setup Docker"
+    "Setup flatpak"
+    "Setup Latex"
     "Setup OpenSCAD"
     "Setup KiCAD"
-    "Setup Latex"
-    "Setup LF"
-    "Setup GIMP"
-    "Setup Cura"
+    "Setup VSCode (latex, openscad)"
     "Setup Virtualbox"
-    "Setup Kind"
+    "Setup Docker"
+    "Setup Kind (docker)"
+    "Setup GIMP"
+    "Setup Cura (flatpak)"
+    "Setup AATTS"
     "Run all commands"
     "Exit"
 )
@@ -101,23 +100,24 @@ welcome() {
     Fedora installation script
 
     '
-    prompt "Do you wish to proceed?" $(echo "proceeding...") $(exit 0)
+    prompt "Do you wish to proceed?" 'echo "proceeding..."' 'exit 0'
 }
 
-#######################################
-#   Set the git email and user name.
-# Globals:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   Inputs for email and name
-#######################################
-gitConf() {
+
+##############################################################################
+########################  Installation and setup #############################
+##############################################################################
+
+setup_git() {
+    if [[ -n "$(git config user.name)" ]]; then
+        echo "Exiting from the function"
+        return
+    fi
+
     read -r -p "\nGit email: " gitEmail
     read -r -p "Git name: " gitName
 
-    prompt "Confinnue with $gitEmail and $gitNme" $(echo "confinuing") $(exit 0)
+    prompt "Configure with $gitEmail and $gitName" $(echo "git configured") $(exit 0)
 
     sudo -u $USER git config --global user.email $gitEmail
     sudo -u $USER git config --global user.name $gitName
@@ -125,31 +125,16 @@ gitConf() {
     add_message "Generate keys and add keys to GitHub account"
 }
 
-#######################################
-#   Some general dependencies
-# Globals:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   None
-#######################################
 dependencies() {
-    sudo zypper update
-    mkdir $HOME/Programs/
+    sudo zypper update 
+    sudo zypper install git
+    mkdir -p $HOME/Programs/
+
+    setup_git
 }
 
-#######################################
-#   Setup zsh
-# Globals:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   None
-#######################################
-terminalApplication() {
-    sudo zypper install zsh
+setup_zsh() {
+    sudo zypper install zsh lf
 
     # Enable zsh
     sudo lchsh $USER
@@ -168,19 +153,31 @@ terminalApplication() {
     zsh -c 'git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode'
 }
 
-#######################################
-#   Setup VSCode
-# Globals:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   None
-#######################################
-vscode() {
-    sudo zypper install code openscad texlive-latex
+setup_flatpak() {
+    sudo zypper install flatpak
+    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-    # VS_Code extensions 
+    add_message "Reboot before installing flatpak packages"
+}
+
+setup_latex() {
+    sudo zypper install texlive-latex
+}
+
+setup_openscad() {
+    sudo zypper install openscad
+}
+
+setup_kicad() {
+    sudo zypper addrepo https://download.opensuse.org/repositories/electronics/openSUSE_Tumbleweed/electronics.repo
+    sudo zypper refresh
+    sudo zypper install kicad
+}
+
+setup_vscode() {
+    sudo zypper install code
+
+    # VS_Code extensions
     code --install-extension vscodevim.vim
     code --install-extension james-yu.latex-workshop
     code --install-extension hediet.vscode-drawio
@@ -194,19 +191,54 @@ vscode() {
     #"vim.useSystemClipboard": true,
 }
 
-#######################################
-#   Install AATTS
-# Globals:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   None
-#######################################
-tts() {
+setup_virtualbox() {
+    sudo zypper update
+    sudo zypper install virtualbox
+    sudo gpasswd -a $USER vboxusers
+}
+
+setup_docker() {
+    sudo zypper install docker docker-compose docker-compose-switch
+    sudo systemctl enable docker
+    sudo usermod -G docker -a $USER
+    newgrp docker
+    sudo systemctl restart docker
+    sudo systemctl start docker
+}
+
+setup_kind() {
+    #   https://kind.sigs.k8s.io/docs/user/quick-start/
+    #   https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+
+    [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
+
+    chmod +x ./kind
+    sudo mv ./kind /usr/local/bin/kind
+
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    kubectl version --client
+}
+
+setup_gimp() {
+    sudo zypper install gimp
+}
+
+setup_cura() {
+    #   https://flathub.org/apps/com.ultimaker.cura
+    sudo flatpak install flathub com.ultimaker.cura
+}
+
+setup_tts() {
     git clone git@github.com:Anton-Augustsson/tts.git $HOME/Programs/tts/
     make -C $HOME/Programs/tts/
 }
+
+##############################################################################
+##########################  To run the scripts  ##############################
+##############################################################################
 
 #######################################
 #   Display a menu of the avalible commands
@@ -239,55 +271,66 @@ execute_commands() {
         case $index in
             1 )
                 echo "${commands[$index - 1]}"
-                setup_git_command
+                setup_zsh
                 ;;
             2 )
                 echo "${commands[$index - 1]}"
-                setup_zsh_command
+                setup_flatpak
                 ;;
             3 )
                 echo "${commands[$index - 1]}"
-                setup_vscode_command
+                setup_latex
                 ;;
             4 )
                 echo "${commands[$index - 1]}"
-                setup_docker_command
+                setup_openscad
                 ;;
             5 )
                 echo "${commands[$index - 1]}"
-                setup_openscad_command
+                setup_kicad
                 ;;
             6 )
                 echo "${commands[$index - 1]}"
-                setup_kicad_command
+                setup_vscode
                 ;;
             7 )
                 echo "${commands[$index - 1]}"
-                setup_latex_command
+                setup_virtualbox
                 ;;
             8 )
                 echo "${commands[$index - 1]}"
-                setup_lf_command
+                setup_docker
                 ;;
             9 )
                 echo "${commands[$index - 1]}"
-                setup_gimp_command
+                setup_kind
                 ;;
             10 )
                 echo "${commands[$index - 1]}"
-                setup_cura_command
+                setup_gimp
                 ;;
             11 )
                 echo "${commands[$index - 1]}"
-                setup_virtualbox_command
+                setup_cura
                 ;;
             12 )
                 echo "${commands[$index - 1]}"
-                setup_kind_command
+                setup_tts
                 ;;
             13 )
                 echo "${commands[$index - 1]}"
-                run_all_commands
+                setup_zsh
+                setup_flatpak
+                setup_latex
+                setup_openscad
+                setup_kicad
+                setup_vscode
+                setup_virtualbox
+                setup_docker
+                setup_kind
+                setup_gimp
+                setup_cura
+                setup_tts
                 ;;
             14 )
                 echo "Exiting..."
@@ -301,6 +344,7 @@ execute_commands() {
 }
 
 welcome
+dependencies
 while true; do
     display_menu
 
